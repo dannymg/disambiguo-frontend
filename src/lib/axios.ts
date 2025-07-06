4162cffd2ac76af3654a09af13a78d5bc5f9400f
dannymg <dannymg2002@gmail.com>
2025-07-06 09:10:27 -0500

    Cambios para Deploy

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337/api",
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:1337/api",
   headers: {
     "Content-Type": "application/json",
   },
